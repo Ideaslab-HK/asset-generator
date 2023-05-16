3122d0ad695da00d6b7811d23eed1c26e1bfb68f
Anton <anton@ideaslab.hk>
2023-05-16 11:27:23 +0800

    Supports input files in @4x / xxxhdpi resolution

--- generate.js
+++ generate.js
@@ -20,35 +20,52 @@ function resizeAndSave(factor, source, target) {
     });
 }
 
-function generate(files, targetAndroid, targetIos) {
+function generate(files, targetAndroid, targetIos, scale) {
     for (let file of files) {
         let filename = path.basename(file);
         let filetype = filename.substr(filename.lastIndexOf('.'), filename.length);
+        let effectiveScale = scale
 
         filename = filename.substr(0, filename.lastIndexOf('.'));
         if (filename.indexOf("@3x")!==-1) {
+        	effectiveScale = 3;
             filename = filename.substr(0, filename.lastIndexOf('@'));
         }
 
         console.info("process " + filename + filetype);
 
         if (typeof targetAndroid !== "undefined") {
-            resizeAndSave(0.5, file, targetAndroid + 'drawable-hdpi/' + filename.toLowerCase() + filetype);
-            resizeAndSave(0.25, file, targetAndroid + 'drawable-ldpi/' + filename.toLowerCase() + filetype);
-            resizeAndSave(1 / 3, file, targetAndroid + 'drawable-mdpi/' + filename.toLowerCase() + filetype);
-            resizeAndSave(2 / 3, file, targetAndroid + 'drawable-xhdpi/' + filename.toLowerCase() + filetype);
-            resizeAndSave(1, file, targetAndroid + 'drawable-xxhdpi/' + filename.toLowerCase() + filetype);
+        	if (effectiveScale == 3) {
+				resizeAndSave(0.5, file, targetAndroid + 'drawable-hdpi/' + filename.toLowerCase() + filetype);
+				resizeAndSave(0.25, file, targetAndroid + 'drawable-ldpi/' + filename.toLowerCase() + filetype);
+				resizeAndSave(1 / 3, file, targetAndroid + 'drawable-mdpi/' + filename.toLowerCase() + filetype);
+				resizeAndSave(2 / 3, file, targetAndroid + 'drawable-xhdpi/' + filename.toLowerCase() + filetype);
+				resizeAndSave(1, file, targetAndroid + 'drawable-xxhdpi/' + filename.toLowerCase() + filetype);
+        	} else {
+   	            resizeAndSave(0.75 / 4, file, targetAndroid + 'drawable-ldpi/' + filename.toLowerCase() + filetype);
+				resizeAndSave(1 / 4, file, targetAndroid + 'drawable-mdpi/' + filename.toLowerCase() + filetype);
+				resizeAndSave(1.5 / 4, file, targetAndroid + 'drawable-hdpi/' + filename.toLowerCase() + filetype);
+				resizeAndSave(2 / 4, file, targetAndroid + 'drawable-xhdpi/' + filename.toLowerCase() + filetype);
+				resizeAndSave(3 / 4, file, targetAndroid + 'drawable-xxhdpi/' + filename.toLowerCase() + filetype);
+				resizeAndSave(1, file, targetAndroid + 'drawable-xxxhdpi/' + filename.toLowerCase() + filetype);
+			}
         }
 
         if (typeof targetIos !== "undefined") {
-            resizeAndSave(1/3, file, targetIos + filename + filetype);
-            resizeAndSave(2/3, file, targetIos + filename + '@2x' + filetype);
-            resizeAndSave(1, file, targetIos + filename + '@3x' + filetype);
+        	if (effectiveScale == 3) {
+				resizeAndSave(1/3, file, targetIos + filename + filetype);
+				resizeAndSave(2/3, file, targetIos + filename + '@2x' + filetype);
+				resizeAndSave(1, file, targetIos + filename + '@3x' + filetype);
+        	} else {
+				resizeAndSave(1 / 4, file, targetIos + filename + filetype);
+				resizeAndSave(1 / 2, file, targetIos + filename + '@2x' + filetype);
+				resizeAndSave(3 / 4, file, targetIos + filename + '@3x' + filetype);
+        	}
         }
     }
 }
 
-module.exports = (q, targetAndroid, targetIos) => {
+module.exports = (q, targetAndroid, targetIos, scale) => {
     if (typeof targetAndroid !== "undefined") {
         targetAndroid = ensureTrailingSlash(targetAndroid);
         fse.ensureDirSync(targetAndroid + 'drawable-hdpi');
@@ -56,6 +73,9 @@ module.exports = (q, targetAndroid, targetIos) => {
         fse.ensureDirSync(targetAndroid + 'drawable-mdpi');
         fse.ensureDirSync(targetAndroid + 'drawable-xhdpi');
         fse.ensureDirSync(targetAndroid + 'drawable-xxhdpi');
+//         if (typeof scale !== "undefined") {
+// 	        fse.ensureDirSync(targetAndroid + 'drawable-xxxhdpi');
+//         }
     }
 
     if (typeof targetIos !== "undefined") {
@@ -64,8 +84,8 @@ module.exports = (q, targetAndroid, targetIos) => {
     }
 
     if (q.length == 1 && q[0].indexOf("*") !== -1) {
-        glob(q[0], {}, (er, files) => generate(files, targetAndroid, targetIos));
+        glob(q[0], {}, (er, files) => generate(files, targetAndroid, targetIos, scale));
     } else {
-        generate(q, targetAndroid, targetIos);
+        generate(q, targetAndroid, targetIos, scale);
     }
 }
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ program
   .usage('[options] <file ...>')
   .option('-a, --android [path]', 'android path')
   .option('-i, --ios [path]', 'iOS Path')
+  .option('-x, --scale [3|4]', 'input scale is @3x (iOS), xxhdpi (Android). Default to @4x, xxxhdpi.')
   .parse(process.argv);
 
-generate(program.args, program.android, program.ios);
+generate(program.args, program.android, program.ios, program.scale);
